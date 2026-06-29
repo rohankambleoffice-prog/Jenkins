8976d4a1e14d9bacc906fc29297fe3de96ac540f
rohankambleoffice-prog <rohankambleoffice@gmail.com>
2026-06-30 01:49:18 +0530

    Enhance root route response with service links tag:1.1.4
    Updated the response message for the root route to include additional links.

--- src/app.js
+++ src/app.js
@@ -2,7 +2,12 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.status(200).send("🚀 My Node App Version 1.1.2 running with Jenkins CI/CD");
+    res.status(200).send('''🚀 My Node App Version 1.1.2 running with Jenkins CI/CD \
+        Here is my sonarqube : http://80.65.208.53:9000 \
+        Here is my Prmetheus : http://80.65.208.53:9090 \
+        Here is my Grafana : http://80.65.208.53:3001 \
+        And Here is my App : http://80.65.208.53:3000
+                         ''');
 });
 
 app.get("/health", (req, res) => {
